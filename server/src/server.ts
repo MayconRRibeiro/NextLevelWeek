7db8a5e95f097fac661aae12834fbc9d7ec30463
Mike <m1kros@hotmail.com>
2020-06-05 15:28:31 -0300

    Limpando comentários sobre HTTP/Rotas/Recursos/Parameters.

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,22 +5,6 @@ const app = express();
 // Possibilita ao Express poder ler JSON
 app.use(express.json());
 
-// Rotas: Endereço completo da requisição.
-// Recurso: Qual entidade estamos acessando no sistema.
-
-// GET: Buscar uma ou mais informações do back-end
-// POST: Criar uma nova informação no back-end
-// PUT: Atualizar uma informação existente no back-end
-// DELETE: Remover uma informação do back-end
-
-// POST http://localhost:3333/users = Criar um usuário
-// GET http://localhost:3333/users = Listar usuários
-// GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
-
-// Request Param: Parâmetros obrigatorios que vem na própria rota que identificam um recurso
-// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
-// Request Body: Parâmetros para criação/atualização de informações
-
 const users = ["Mike", "Cleriton", "Robson", "Daniel"];
 
 app.get("/users", (request, response) => {
