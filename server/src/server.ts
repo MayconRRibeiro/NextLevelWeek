3ac9578e49ddc1176e5bb18f66f529fa53320739
Mike <m1kros@hotmail.com>
2020-06-05 15:27:12 -0300

    #2 Rotas e recursos, Métodos HTTP e Tipos de parâmetros

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,56 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
+// Possibilita ao Express poder ler JSON
+app.use(express.json());
 
-  response.json(["Mike", "Cleriton", "Robson", "Daniel"]);
+// Rotas: Endereço completo da requisição.
+// Recurso: Qual entidade estamos acessando no sistema.
+
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
+
+// Request Param: Parâmetros obrigatorios que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualização de informações
+
+const users = ["Mike", "Cleriton", "Robson", "Daniel"];
+
+app.get("/users", (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  return response.json(filteredUsers);
+});
+
+// : -> significa que estou recebendo um parâmetro
+// :batata -> request.params.batata
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
