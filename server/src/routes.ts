7d0e3326b7b048b0237c8cc1f452e8e9d51fb984
Mike <m1kros@hotmail.com>
2020-06-06 20:47:54 -0300

    #2 Listar pontos de coleta aplicando filtro
    Requisição Post de Points estava com problema, não cadastrava por falta de add -> await trx.commit();

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,9 @@ const itemsController = new ItemsController();
 
 // index, show, create, update, delete
 routes.get("/items", itemsController.index);
+
 routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
 export default routes;
