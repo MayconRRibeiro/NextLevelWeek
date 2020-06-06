e27078c0bfe38ce3520c857399b1264ac5b3ab96
Mike <m1kros@hotmail.com>
2020-06-06 18:32:50 -0300

    #2 Listar items de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+routes.get("/items", async (request, response) => {
+  // Sempre que for utilizado uma query do banco de dados, precisamos utilizar
+  // async/await para poder aguardar os resultados
+  const items = await knex("items").select("*");
+
+  // Serialização de dados, eu transformo os dados para um novo formato o qual
+  // vai ser mais accesível para quem esta requisitando essas informações.
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
