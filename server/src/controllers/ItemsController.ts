6dda42c3f0f72987a049d30a61ad086bb31c0ba2
Mike <m1kros@hotmail.com>
2020-06-06 20:26:42 -0300

    #2 Extraindo Requests para controller

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import knex from "../database/connection";
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    // Sempre que for utilizado uma query do banco de dados, precisamos utilizar
+    // async/await para poder aguardar os resultados
+    const items = await knex("items").select("*");
+
+    // Serialização de dados, eu transformo os dados para um novo formato o qual
+    // vai ser mais accesível para quem esta requisitando essas informações.
+    const serializedItems = items.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://localhost:3333/uploads/${item.image}`,
+      };
+    });
+
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;
